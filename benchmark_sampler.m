c3a32ad83b783fced2763667f5bc81f4a757dad2
Pedro Saa <32940774+psaahiguera@users.noreply.github.com>
2019-03-09 13:04:01 -0300

    Update benchmark_sampler.m

--- benchmark_sampler.m
+++ benchmark_sampler.m
@@ -1,27 +1,25 @@
 clc,clearvars,close all
 
-% Parameter sampling
-% RT  = 8.314*293.15/1e3;
-% DGr = -10;            % kJ/mol
+% Equilibrium constant of TPI
 Keq = 3e8/6.5e5;       % From Brown & Cooper 1993 (~ -15 kJ/mol)
 
-% Based on Bar-Even paper (kcat 1/s, Km M)
+% Bounds on kinetic parameters. Based mostly on Bar-Even paper (kcat 1/s, Km M)
 ub = [1e+4,1e+4,1e-3,1e-3,Keq];
 lb = [1e+0,1e+0,1e-6,1e-6,Keq];
 
 % Define convex space (constraints)
-Aeq = [1,-1,-1,1,-1];
+Aeq = [1,-1,-1,1,-1];      % Haldane relationship
 beq = 0;
 % Aineq = [];
 % bineq = [];
-Aineq = [1,0,-1,0,0;...
+Aineq = [1,0,-1,0,0;...    % Diffusion limit
          0,1,0,-1,0];
 bineq = log([1e9;1e9]);
 
 % Determine bounding box
 [box,xinit,A,b] = calculateBoundingBox(Aeq,beq,Aineq,bineq,log(ub),log(lb));
 
-% %% Prior selection
+% %% Prior hyperparameters
 mu1    = log(16e-6);
 sigma1 = 0.2;
 mu2    = log(4827);
@@ -29,14 +27,14 @@ sigma2 = 0.2;
 mu3    = log(560e-6);
 sigma3 = 0.2;
 
-% Choices
+% Prior choices
 % priorFxn = @(point) uniform_prior(point);               
 % priorFxn = @(point) logNormal_prior1(point,mu1,sigma1);
 % priorFxn = @(point) logNormal_prior2(point,mu1,sigma1,mu2,sigma2);
 priorFxn = @(point) logNormal_prior3(point,mu1,sigma1,mu2,sigma2,mu3,sigma3);
 
 %% Sampling
-% General Hit And Run Sampler
+% Define parameters of the General Hit And Run Sampler
 nSamples = 2e4;
 nSteps   = 2e2;
 nDiscard = 1e5;
